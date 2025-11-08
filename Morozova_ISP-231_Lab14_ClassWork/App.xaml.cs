57bebbcb8de3a5c36250583f261f1e42ee3a3e86
MorOlesya <dl9morozova@gmail.com>
2025-11-08 08:44:07 +0300

    Add project files.

---
+++ Morozova_ISP-231_Lab14_ClassWork/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Morozova_ISP_231_Lab14_ClassWork
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
