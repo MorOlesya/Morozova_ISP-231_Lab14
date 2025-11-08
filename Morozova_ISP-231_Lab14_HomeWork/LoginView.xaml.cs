9154e0c3696df812aeb70a7c7bf1a8d3b2f03226
MorOlesya <dl9morozova@gmail.com>
2025-11-08 12:43:07 +0300

    finish

---
+++ Morozova_ISP-231_Lab14_HomeWork/LoginView.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Morozova_ISP_231_Lab14_HomeWork
+{
+    public partial class LoginView : UserControl
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (TextName.Text == "student" && Password.Password.ToString() == "pass123")
+            {
+                MessageBox.Show("Добро пожаловать, student!");
+                TextName.Clear();
+                Password.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Неверные данные");
+            }
+        }
+    }
+}
--- Morozova_ISP-231_Lab14_HomeWork/MainWindow.xaml.cs
+++ Morozova_ISP-231_Lab14_HomeWork/MainWindow.xaml.cs
@@ -11,14 +11,12 @@ using System.Windows.Shapes;
 
 namespace Morozova_ISP_231_Lab14_HomeWork
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            MainContent.Content = new LoginView();
         }
     }
 }
